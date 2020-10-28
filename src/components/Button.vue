fdc2abf174a87dfecfd4ad8e710cc87e006aeb21
khanr <khanriza@gmail.com>
2020-10-27 22:25:53 -0400

    First Commit

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="button" :class="{button.conditionClass}">
+    {{ button.title }}
+  </button>
+</template>
+
+
+<script>
+export default {
+  props: ['button']
+}
+</script>
+
+<style>
+  .button {
+    border: solid 2px red;
+    padding: 1rem 1.75rem;
+  }
+  .success {
+    background: green;
+  }
+
+  .danger {
+    background: red;
+  }
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Button :button="button">
   </div>
 </template>
+
+<script>
+import Button from '@/components/Button'
+
+export default { 
+  components: { Button },
+  data() {
+    return {
+      button: {
+        class: 'success',
+        title: 'From About'
+      }
+    }
+  }
+
+}
+</script>
